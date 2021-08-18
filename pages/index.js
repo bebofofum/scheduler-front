d1dc0a40371a29b88d3ca3760ddc4e0cb933ad61
Brian Boehm <bri.boehm@gmail.com>
2021-08-18 16:22:26 -0400

    Including Links module methods for navigating.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home({ streamData }) {
@@ -14,6 +15,8 @@ export default function Home({ streamData }) {
             <div className="">{Description}</div>
          </div>
       ))}
+      {/* Testing How Next does page Links */}
+      <h2>See <Link href="/streams/first-stream"><a>First Stream</a></Link></h2>
      
     </div>
   )
@@ -26,6 +29,8 @@ export async function getStaticProps() {
   const getStreamsRes = await fetch("http://localhost:1337/streams");
 
   const streamData = await getStreamsRes.json();
+
+  console.log(streamData)
 
 
   return {
