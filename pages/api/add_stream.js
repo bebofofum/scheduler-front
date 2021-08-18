2855dc87cce2795a44fba798d6afcb814bf9f67d
Brian Boehm <bri.boehm@gmail.com>
2021-08-18 16:20:14 -0400

    Working on next pages for naviating

---
+++ pages/api/add_stream.js
@@ -0,0 +1,17 @@
+function AddStream() {
+
+    
+    return (
+       <form>
+           <label htmlFor="name">Stream Name</label>
+           <input 
+                className="" 
+                id="name" 
+                name="name" 
+                type="text"
+                placeholder="Stream Name" />
+       </form>
+    )
+}
+
+export default AddStream;
