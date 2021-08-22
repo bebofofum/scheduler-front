4a38132ee33c0e7e456cf67c59690374deda97e8
Brian Boehm <bri.boehm@gmail.com>
2021-08-22 17:39:21 -0400

    Building a create stream form page, see how it can update strapi

--- pages/create-stream.js
+++ pages/create-stream.js
@@ -1,3 +1,6 @@
+import { Typography, Card, CardContent, Grid, TextField, FormGroup } from '@material-ui/core';
+import styles from '../styles/Form.module.css'
+
 
 function CreateStream() {
 
@@ -5,14 +8,23 @@ function CreateStream() {
     return (
         <div className="container">
             <div className="form-box">
+                <h5 className="form-title">Enter a Stream Schedule</h5>
                 <form>
                     <label htmlFor="name">Stream Name</label>
                     <input 
-                            className="" 
-                            id="name" 
-                            name="name" 
-                            type="text"
-                            placeholder="Stream Name" />
+                        className="" 
+                        id="name" 
+                        name="name" 
+                        type="text"
+                        placeholder="Stream Name" />
+                    <label htmlFor="description">Stream Description</label>
+                    <input 
+                        className="" 
+                        id="description" 
+                        name="description" 
+                        type="text"
+                        placeholder="Stream Details" />
+
                 </form>
         </div>
        </div>
